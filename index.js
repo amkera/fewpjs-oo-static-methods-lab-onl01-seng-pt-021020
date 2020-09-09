9c81ae5823a37a21d908879ffcb60045077d5be3
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-09 02:42:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 class Formatter {
-  static capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1); 
+  static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
   }
-  
-  static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9-']+/g, '')
+
+  static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
   }
-  
-   static titleize( sentence ) {
+
+  static titleize( sentence ) {
     let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let result = [];
     let arrayOfWords = sentence.split( " " )
